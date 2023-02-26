ef27b36d696a605a07e3c1e45706f4de02b16043
ReshitkoM <73237232+ReshitkoM@users.noreply.github.com>
2023-02-27 00:26:30 +0300

    REFACTOR: move mq creation to processor

--- main.go
+++ main.go
@@ -6,12 +6,8 @@ import (
 	"fmt"
 
 	"os"
-	"time"
-
-	"context"
 	"log"
 
-	amqp "github.com/rabbitmq/amqp091-go"
 	"gopkg.in/yaml.v3"
 )
 
@@ -34,47 +30,10 @@ type SendMsg struct {
 }
 
 func (app *App) StartBot() {
-	var err error
-	// defer channel.Close()
-
 	app.tg_bot = newTelegram(app.config.Bot.Token)
-	//MQ
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+	app.proc = newProcessor()
+	defer app.stop()
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-	// app.mq_channel = ch
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		true,    // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-	// app.mq_queue = &q
-	_, cancelmq := context.WithTimeout(context.Background(), 5*time.Second)
-	// app.mq_context = &ctx
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-	// app.mq_consumer = msgs
-	defer cancelmq()
-
-	app.proc = &processor{&q, ch, msgs}
 	// Pass cancellable context to goroutine
 	go app.receiveUpdates()
 	// Tell the user the bot is online
@@ -82,9 +41,6 @@ func (app *App) StartBot() {
 
 	// Wait for a newline symbol, then cancel handling updates
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
-
-	app.tg_bot.stop()
-
 }
 
 func (app *App) receiveUpdates() {
@@ -117,6 +73,11 @@ func (app *App) receiveUpdates() {
 			// failOnError(err, "Failed to send msg to user")
 		}
 	}
+}
+
+func (app *App) stop() {
+	app.tg_bot.stop()
+	app.proc.stop()
 }
 
 // config
--- processor.go
+++ processor.go
@@ -9,9 +9,11 @@ import (
 )
 
 type processor struct {
+	mq_connection *amqp.Connection
 	mq_queue    *amqp.Queue
 	mq_channel  *amqp.Channel
 	mq_consumer <-chan amqp.Delivery
+	mq_ctxCancel context.CancelFunc
 }
 
 func (proc *processor) sendVoiceToTextReq(voice []byte, chatId int64) {
@@ -43,4 +45,46 @@ func (proc *processor) processReqResult(msg amqp.Delivery) (chatID int64, text s
 	failOnError(err, "Failed to decode msg")
 	text = s.Text
 	return chatID, text
+}
+
+func (proc *processor) stop() {
+	proc.mq_connection.Close()
+	proc.mq_channel.Close()
+	proc.mq_ctxCancel()
+}
+
+func newProcessor() *processor {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	// defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	// defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		true,    // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	// app.mq_queue = &q
+	_, cancelmq := context.WithTimeout(context.Background(), 5*time.Second)
+	// app.mq_context = &ctx
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+	// defer cancelmq()
+	return &processor{conn, &q, ch, msgs, cancelmq}
 }
